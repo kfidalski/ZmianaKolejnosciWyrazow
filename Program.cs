9659e4d37df2a98a56f295086eeabb7aefabee8b
kfidalski <48648696+kfidalski@users.noreply.github.com>
2019-11-05 13:09:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            string[] data = input.ToString().Split();
+            string[] newString = new string[data.Length];
+
+            var len = data.Length;
+         
+            int i, j;
+           
+            for(j = len-1, i = 0; j >= 0; j--, i++)
+            {
+               
+                newString[i] = data[j];
+             //   Console.WriteLine($"{i} :  {newString[i]}");
+
+            }
+
+            string res = string.Join(" ", newString);
+
+            Console.WriteLine($"Output string : {res}");
+
+            Console.ReadKey();
+        }
+    }
+}
